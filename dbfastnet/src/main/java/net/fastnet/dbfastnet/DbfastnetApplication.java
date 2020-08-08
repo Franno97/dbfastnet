3a75e8558ecfdc71cef7185ce076024dc08eab45
franj <franj@LAPTOP-DIRSTBTR>
2020-08-08 13:26:03 -0500

    primer commit

---
+++ dbfastnet/src/main/java/net/fastnet/dbfastnet/DbfastnetApplication.java
@@ -0,0 +1,13 @@
+package net.fastnet.dbfastnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbfastnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbfastnetApplication.class, args);
+	}
+
+}
---
+++ dbfastnet/src/test/java/net/fastnet/dbfastnet/DbfastnetApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fastnet.dbfastnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbfastnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
