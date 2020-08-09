1c223477748ff610997f030fbf9ceb30622dd0a2
franj <franj@LAPTOP-DIRSTBTR>
2020-08-09 18:01:44 -0500

    hola

--- dbfastnet/src/main/java/net/fastnet/dbfastnet/service/UserService.java
+++ dbfastnet/src/main/java/net/fastnet/dbfastnet/service/UserService.java
@@ -5,6 +5,6 @@ import net.fastnet.dbfastnet.controller.UserRegistrationDto;
 import net.fastnet.dbfastnet.entities.User;
 
 public interface UserService extends UserDetailsService {
-	User findByEmail(String email);
-	User save(UserRegistrationDto registration);
+    User findByEmail(String email);
+    User save(UserRegistrationDto registration);
 }
